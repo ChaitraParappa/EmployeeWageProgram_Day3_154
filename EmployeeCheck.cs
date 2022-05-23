b14c2c032fa6ce1ca8087762a92791259ac177f2
Chaitra P1 <chaitra.p1@ibm.com>
2022-05-23 09:28:04 +0530

    Checking Employee is Present or Not

---
+++ EmployeeCheck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    /// <summary>
+    /// Employee Class
+    /// </summary>
+    public class EmployeeCheck
+    {
+        /// <summary>
+        /// The method is checking the Employee is Present or Not.
+        /// </summary>
+        public static void CheckEmployeePresentOrNot()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(2);
+
+            if(empCheck==IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
