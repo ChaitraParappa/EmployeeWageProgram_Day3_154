c860bb9f2ac92b06645ea15ad6f96f2fef96de81
Chaitra P1 <chaitra.p1@ibm.com>
2022-05-23 09:13:38 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Program");
+
+        }
+    }
+}
