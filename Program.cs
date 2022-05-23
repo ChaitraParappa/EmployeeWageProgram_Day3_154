03e7e701667aa6c1c8f43eb7edf17c901de0848d
Chaitra P1 <chaitra.p1@ibm.com>
2022-05-23 09:26:01 +0530

    Adding Employee Check

--- Program.cs
+++ Program.cs
@@ -2,9 +2,15 @@
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage Program");
+
+            EmployeeCheck.CheckEmployeePresentOrNot();
 
         }
     }
