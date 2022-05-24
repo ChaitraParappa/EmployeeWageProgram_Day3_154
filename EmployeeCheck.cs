3f61204e4fe585fe6d62297942adb3ce831a80e9
Chaitra P1 <chaitra.p1@ibm.com>
2022-05-24 08:21:04 +0530

    Added Part Time Employee

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -17,6 +17,7 @@ namespace EmployeeWageProgram
         public static void CheckEmployeePresentOrNot()
         {
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
 
             int EMP_RATE_PER_HOUR = 20;
 
@@ -24,17 +25,19 @@ namespace EmployeeWageProgram
             int empWage = 0;
             Random random = new Random();
 
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
 
             if(empCheck==IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
                 empHrs = 8;
 
             }
+            else if(empCheck==IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
             else
             {
-                Console.WriteLine("Employee is Absent");
                 empHrs = 0;
             }
 
